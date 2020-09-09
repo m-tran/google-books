7e4ba6e321d4d9fe44facedc8cffeb3e416c97ff
Michelle Tran <mptran0101@berkeley.edu>
2020-09-08 20:43:38 -0700

    defied books schema

--- models/Books.js
+++ models/Books.js
@@ -2,8 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BooksSchema = new Schema({
-        
-    }
+    title: {
+        type: String,
+        required: "String is required"
+    },
+    author: {
+        type: String,
+        required: "String is required"
+    },
+    ISBNS: [{
+        isbn10: {
+            type: String
+        },
+        isbn13: {
+            type: String
+        }
+    }]
 });
 
 const Books = mongoose.model("Books", BooksSchema);
